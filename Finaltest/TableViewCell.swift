feaf3a8872092c6ee90c20f9995ee8819b8430a4
Hemanth <hemanthkotla@icloud.com>
2019-12-11 21:27:59 -0500

    Database is created and the values are stored. Able to calculate BMI

---
+++ Finaltest/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Finaltest
+//
+//  Created by Hemanth Kotla on 2019-12-11.
+//  Copyright © 2019 Hemanth Kotla. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
